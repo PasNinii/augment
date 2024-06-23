1811424fc65043266e1b2fe5919c48a508c4cb0e
Nicolas Rannou <nicolas.rannou@spinergie.com>
2024-06-23 09:04:22 +0000

    p

--- mobile-client/env-setup.js
+++ mobile-client/env-setup.js
@@ -1,39 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { exec } = require('child_process');
-
-exec('ls', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout ls: ${stdout}`);
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-  }
-});
-
-exec('pwd', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout pwd: ${stdout}`);
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-  }
-});
-
-exec('echo $ANGULAR_OUTPUT_PATH', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error echo: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-  }
-});
 
 const writeJsonFile = (name = 'environment.json') => {
   const ENVIRONMENT_VARIABLES = {
